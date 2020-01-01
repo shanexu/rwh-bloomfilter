39896f0aba766ffc05b6c9781e6cf003f84462e3
Xu Shane <xusheng0711@gmail.com>
2020-01-02 01:11:42 +0800

    main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
-import Lib
+import BloomFilter.Mutable
+import Control.Monad.ST (stToIO)
+import Prelude hiding (elem)
 
 main :: IO ()
-main = someFunc
+main = do
+  f <- stToIO $ new (\i -> [fromIntegral i `mod` 10]) 10
+  putStrLn "insert:"
+  x1 <- readLn
+  stToIO $ insert f x1
+  putStrLn "insert:"
+  x2 <- readLn
+  stToIO $ insert f x2
+  putStrLn "check:"
+  x3 <- readLn
+  x <- stToIO (x3 `elem` f)
+  putStrLn $
+    if x
+      then "Yes"
+      else "No"
